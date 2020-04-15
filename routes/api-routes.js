d997675d73c7d91ea7432aebe4b820ef79a95a20
Ricardo Russ <ricardoruss@live.com>
2020-04-15 12:17:42 -0400

    added pokemon manager page structure/added in completed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@
 // Requiring our models
 var db = require("../models");
 
-//Gets all teams
+//Gets all teams; include needed in order to provide left outer join info
 module.exports = function(app) {
   app.get("/api/teams", function(req, res) {
     db.Team.findAll({
@@ -20,15 +20,85 @@ module.exports = function(app) {
         id: req.params.id
       },
       include: [db.Pokemon]
-    }).then()
-  })
+    }).then(function(dbTeam){
+      res.json(dbTeam);
+    });
+  });
 
   app.post("/api/teams", function(req, res) {
-    db.Team.create({
-      teamName: req.body.teamName
-    })
+    db.Team.create(req.body)
       .then(function(dbTeam){
         res.json(dbTeam);
       });
+  });
+
+  app.delete("/api/teams/:id", function(req, res) {
+    db.Team.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbTeam){
+      res.json(dbTeam);
+    });
+  });
+
+  app.put("/api/teams", function(req, res) {
+    db.Team.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbTeam){
+      res.json(dbTeam);
+    });
+  });
+
+  app.get("/api/pokemons", function(req, res) {
+    db.Pokemon.findAll({
+      include: [db.Team]
+    }).then(function(dbPokemon){
+      res.json(dbPokemon);
+    });
+  });
+
+  app.get("/api/pokemons/:id", function(req, res) {
+    db.Pokemon.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Team]
+    }).then(function(dbPokemon){
+      res.json(dbPokemon);
+    });
+  });
+
+  app.post("/api/pokemons", function(req, res) {
+    db.Pokemon.create(req.body)
+      .then(function(dbPokemon){
+        res.json(dbPokemon);
+      });
+  });
+
+  app.delete("/api/pokemons/:id", function(req, res) {
+    db.Pokemon.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPokemon){
+      res.json(dbPokemon);
+    });
+  });
+
+  app.put("/api/pokemons", function(req, res) {
+    db.Pokemon.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbPokemon){
+      res.json(dbPokemon);
+    });
   });
 };
