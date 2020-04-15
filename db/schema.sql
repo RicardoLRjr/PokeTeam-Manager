e0b92dc86c54f8a08c83380294703861b84bb88b
Ricardo Russ <ricardoruss@live.com>
2020-04-15 11:50:53 -0400

    basic html structure/building up backend database structure and api routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,36 @@
-DROP DATABASE IF EXISTS `lego_tracker_db`;
-CREATE DATABASE `lego_tracker_db`;
+DROP DATABASE IF EXISTS pokemon_db;
 
-USE lego_tracker_db;
+CREATE DATABASE pokemon_db;
+
+USE pokemon_db;
+
+CREATE TABLE pokemon
+(
+id INT NOT NULL AUTO_INCREMENT, 
+pokedexId INT NOT NULL,
+pokemonName VARCHAR(40) NOT NULL,
+pokeType1 VARCHAR(50) NOT NULL,
+pokeType2 VARCHAR(50), 
+abilities VARCHAR (70) NOT NULL,
+nickname VARCHAR (50) NOT NULL
+PRIMARY KEY (id)
+);
+
+
+CREATE table team
+(
+id INT NOT NULL AUTO_INCREMENT,
+teamName VARCHAR(100) NOT NULL,
+PRIMARY KEY(id)
+),
+
+
+CREATE table teamPokemon
+(
+id INT NOT NULL,
+pokemonId INT NOT NULL,
+teamId INT NOT NULL, 
+PRIMARY KEY (id), 
+FOREIGN KEY (pokemonId) REFERENCES pokemon(pokemonId), 
+FOREIGN KEY (teamId) REFERENCES team(teamId), 
+) 
